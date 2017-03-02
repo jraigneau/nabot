8e6053ff0f22b4b9c028ccc608315f92e419351c
Julien Raigneau <julien@zeneffy.fr>
2017-03-02 21:10:38 +0100

    added crontab

--- nabot.go
+++ nabot.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/jasonlvhit/gocron"
 	forecast "github.com/mlbright/forecast/v2"
 	"log"
 	"os"
@@ -25,6 +26,15 @@ var (
 	botID  = "N/A"
 	logger *log.Logger
 )
+
+//		Création de la crontab
+func ordonnanceur() {
+
+	s := gocron.NewScheduler()
+	s.Every(1).Day().At("07:00").Do(replyWeather, "G4056ALKB")
+	s.Every(1).Day().At("20:00").Do(replyWeather, "G4056ALKB")
+	<-s.Start()
+}
 
 //function d'initialisation (notamment pour récupérer le token) - j'aurai pu utiliser 'init()'
 func initialisation() {
@@ -57,9 +67,8 @@ func initialisation() {
 //Répond à une demande d'aide du bot
 func replyHelp(channelID string) {
 	commands := map[string]string{
-		"aide":     "Affiche la liste des commandes possibles",
-		"identité": "Donne des informations sur @nabot",
-		"météo":    "Affiche la météo à Igny",
+		"aide":  "Affiche la liste des commandes possibles",
+		"météo": "Affiche la météo à Igny",
 	}
 	fields := make([]slack.AttachmentField, 0)
 	for k, v := range commands {
@@ -68,10 +77,13 @@ func replyHelp(channelID string) {
 			Value: v,
 		})
 	}
-	sendMsg("", "", "", fields, "", channelID)
+	title := "Bonjour, je m'appelle @nabot et je suis un bot pour contrôler la domotique de mes maîtres."
+	pretext := "Vous trouverez plus d'information sur https://github.com/jraigneau/nabot"
+	sendMsg(title, pretext, "", fields, "", channelID)
 }
 
 //Répond à la météo
+//TODO: prévoir gestion des erreurs sur les icones pour éviter plantage
 func replyWeather(channelID string) {
 
 	key := botKey.DarkSkyToken
@@ -104,19 +116,12 @@ func replyWeather(channelID string) {
 	//récupération du courant, d'aujourd'hui et des 2 prochains jours
 	title := ""
 	pretext := ""
-	text := fmt.Sprintf("*Cette semaine* à Igny:%s\n\n", f.Daily.Summary)
-	text += fmt.Sprintf("*Actuellement* %s: %s et il fait *%.1f°C*, avec un risque de pluie *%.0f%%* de et un vent à *%.2fm/s*\n\n", icons[f.Currently.Icon], f.Currently.Summary, f.Currently.Temperature, f.Currently.PrecipProbability*100, f.Currently.WindSpeed)
-	text += fmt.Sprintf("*Aujourd'hui* %s: %s et il fera entre *%.1f°C* et *%.1f°C*, avec un risque de pluie de *%.0f%%* et un vent à *%.2fm/s*\n\n", icons[f.Daily.Data[0].Icon], f.Daily.Data[0].Summary, f.Daily.Data[0].TemperatureMin, f.Daily.Data[0].TemperatureMax, f.Daily.Data[0].PrecipProbability*100, f.Daily.Data[0].WindSpeed)
-	text += fmt.Sprintf("*Demain* %s: %s et il fera entre *%.1f°C* et *%.1f°C*, avec un risque de pluie de *%.0f%%* et un vent à *%.2fm/s*\n\n", icons[f.Daily.Data[1].Icon], f.Daily.Data[1].Summary, f.Daily.Data[1].TemperatureMin, f.Daily.Data[1].TemperatureMax, f.Daily.Data[1].PrecipProbability*100, f.Daily.Data[1].WindSpeed)
-	text += fmt.Sprintf("*Et après-demain* %s: %s et il fera entre *%.1f°C* et *%.1f°C*, avec un risque de pluie de *%.0f%%* et un vent à *%.2fm/s*", icons[f.Daily.Data[2].Icon], f.Daily.Data[2].Summary, f.Daily.Data[2].TemperatureMin, f.Daily.Data[2].TemperatureMax, f.Daily.Data[2].PrecipProbability*100, f.Daily.Data[2].WindSpeed)
+	text := fmt.Sprintf("*Cette semaine* à Igny: %s\n\n", f.Daily.Summary)
+	text += fmt.Sprintf("*Actuellement* %s: %s et il fait *%.1f°C*, avec un risque de pluie de *%.0f%%* et un vent à *%.0fm/s*\n\n", icons[f.Currently.Icon], f.Currently.Summary, f.Currently.Temperature, f.Currently.PrecipProbability*100, f.Currently.WindSpeed)
+	text += fmt.Sprintf("*Aujourd'hui* %s: %s et il fera entre *%.1f°C* et *%.1f°C*, avec un risque de pluie de *%.0f%%* et un vent à *%.0fm/s*\n\n", icons[f.Daily.Data[0].Icon], f.Daily.Data[0].Summary, f.Daily.Data[0].TemperatureMin, f.Daily.Data[0].TemperatureMax, f.Daily.Data[0].PrecipProbability*100, f.Daily.Data[0].WindSpeed)
+	text += fmt.Sprintf("*Demain* %s: %s et il fera entre *%.1f°C* et *%.1f°C*, avec un risque de pluie de *%.0f%%* et un vent à *%.0fm/s*\n\n", icons[f.Daily.Data[1].Icon], f.Daily.Data[1].Summary, f.Daily.Data[1].TemperatureMin, f.Daily.Data[1].TemperatureMax, f.Daily.Data[1].PrecipProbability*100, f.Daily.Data[1].WindSpeed)
+	text += fmt.Sprintf("*Et après-demain* %s: %s et il fera entre *%.1f°C* et *%.1f°C*, avec un risque de pluie de *%.0f%%* et un vent à *%.0fm/s*", icons[f.Daily.Data[2].Icon], f.Daily.Data[2].Summary, f.Daily.Data[2].TemperatureMin, f.Daily.Data[2].TemperatureMax, f.Daily.Data[2].PrecipProbability*100, f.Daily.Data[2].WindSpeed)
 	sendMsg(title, pretext, text, nil, "", channelID)
-}
-
-//Répond à l'identité du bot
-func replyIdentity(channelID string) {
-	title := "Bonjour, je m'appelle @nabot et je suis un bot pour contrôler la domotique de mes maîtres"
-	pretext := "Vous trouverez plus d'information sur https://github.com/jraigneau/nabot"
-	sendMsg(title, pretext, "", nil, "", channelID)
 }
 
 //Analyse des messages
@@ -128,12 +133,10 @@ func msgAnalysis(msg string, channelID string) {
 	switch msg {
 	case "<@" + botID + "> aide":
 		replyHelp(channelID)
-	case "<@" + botID + "> identité":
-		replyIdentity(channelID)
 	case "<@" + botID + "> météo":
 		replyWeather(channelID)
 	default:
-		sendMsg("", "", "Désolé je n'ai pas reconnu la commande\nUtiliser `@nabot aide` pour avoir plus d'information", nil, "#FF0000", channelID)
+		sendMsg("", "", "Désolé je n'ai pas reconnu la commande\nUtilisez `@nabot aide` pour avoir plus d'information", nil, "#FF0000", channelID)
 	}
 
 }
@@ -174,8 +177,12 @@ func main() {
 	slack.SetLogger(logger)
 	api.SetDebug(false)
 
+	go ordonnanceur()
+
 	rtm := api.NewRTM()
 	go rtm.ManageConnection()
+
+	var firstConnection = 0
 
 	for msg := range rtm.IncomingEvents {
 		//fmt.Print("Event Received: ")
@@ -186,7 +193,10 @@ func main() {
 		case *slack.ConnectedEvent:
 			logger.Print("Infos:", ev.Info)
 			botID = ev.Info.User.ID
-			sendMsg("Heigh-ho, heigh-ho je vais au boulot", "", "", nil, "", "G4056ALKB")
+			if firstConnection == 0 {
+				sendMsg("", "", "Heigh-ho, heigh-ho je vais au boulot", nil, "", "G4056ALKB")
+				firstConnection = 1 //pour éviter de renvoyer le message avec une connexion instable...
+			}
 
 		case *slack.MessageEvent:
 			if ev.Type == "message" && strings.HasPrefix(ev.Text, "<@"+botID+">") {
